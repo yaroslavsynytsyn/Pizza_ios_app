f6fa0fb793ea102fe405e225ea92deeca8a39ec6
Yaroslav Synytsyn <sinitsyn.ya@gmail.com>
2019-05-13 13:56:51 +0300

    Initial Commit

---
+++ Pizza_ios_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pizza_ios_app
+//
+//  Created by Yaroslav on 5/13/19.
+//  Copyright © 2019 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
